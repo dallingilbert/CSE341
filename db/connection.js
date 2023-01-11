38a6babaf731dd3fd380380d6fa34b5c70974f8e
Dallin Gilbert <djgilb24@byui.edu>
2023-01-10 17:29:30 -0700

    seperating MongoDB connection and server.js

--- db/connection.js
+++ db/connection.js
@@ -3,17 +3,34 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 // Connection URI
-const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@webbackenddev.zwlai5b.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri);
+const mongoURL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@webbackenddev.zwlai5b.mongodb.net/?retryWrites=true&w=majority`;
 
-const connectDB = async() => {
-    try {
-        await client.connect();    
-    } catch (e) {
-        console.error(e);
-    }finally {
-        await client.close();
+let _db; 
+
+const initDb = cb => {
+    if (_db) {
+        console.log('Db is already initialized!');
+        return cb(null, _db);
     }
-}
+    
+    MongoClient.connect(mongoURL)
+        .then(client => {
+            _db = client;
+            cb(null, _db);
+        })
+        .catch(err => {
+            cb(err);
+        });
+};
 
-module.exports = { connectDB };
+const getDb = () => {
+    if (!_db) {
+        throw Error('Db not initialized');
+    }
+    return _db;
+};
+
+module.exports = {
+    initDb,
+    getDb
+ };
